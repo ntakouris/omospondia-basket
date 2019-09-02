f098d81371bc5ecf6c4615064a624712bd2133a5
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-09-02 18:31:57 +0300

    Add stub for katagelies

---
+++ site/src/pages/Katagelies.vue
@@ -0,0 +1,35 @@
+<template>
+  <Layout :title="title">   
+    <section>
+        Προσεχώς
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: 'Katagelies',
+  data () {
+    return {
+      title: 'Δημόσιες Καταγγελίες'
+    }
+  },
+  metaInfo () {
+    return {
+      title: this.title,
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: this.title },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: this.title },
+        { name: 'twitter:site', content: '@v_ntakouris' },
+        { name: 'twitter:creator', content: '@v_ntakouris' }
+      ]
+    }
+  }
+}
+</script>
+
+
+<style>
+</style>
