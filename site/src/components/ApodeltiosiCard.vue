b333eb7b069446bdbce8affbcc9b31b7125ed483
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-09-23 18:20:25 +0300

    add accumulated articles

---
+++ site/src/components/ApodeltiosiCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card">
+    <md-card class="md-elevation-4 diadikasia-card" style="margin-left: 0;">
+      <md-ripple>
+        <md-card-header>
+          <a :href="apodeltiosi.href" target="_blank" :id="apodeltiosi.slug">
+            <div class="md-title">{{ apodeltiosi.display }}</div>
+          </a>
+
+          <div class="md-subhead">{{ apodeltiosi.author }} - {{ apodeltiosi.date }}</div>
+        </md-card-header>
+      </md-ripple>
+
+      <md-card-content v-if="apodeltiosi.content">
+        {{ apodeltiosi.content }}
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button @click="openSharingDialog()">Μοιρασου</md-button>
+        <md-button class="md-icon-button" @click="openSharingDialog()">
+          <md-icon>share</md-icon>
+        </md-button>
+      </md-card-actions>
+    </md-card>
+
+    <SocialShareDialog :title="apodeltiosi.display" :url="getSharingUrl()" :trigger="triggerDialog" />
+  </div>
+</template>
+
+<script>
+import config from "~/data/config.json";
+import SocialShareDialog from "./SocialShareDialog.vue";
+
+export default {
+  name: "ApodeltiosiCard",
+  components: {
+    SocialShareDialog
+  },
+  props: ["apodeltiosi"],
+  data() {
+    return {
+      triggerDialog: false
+    };
+  },
+  methods: {
+    getSharingUrl() {
+      const base = config.baseUrl;
+      const url = `${base}/apodeltiosi#${this.apodeltiosi.slug}`;
+      return url;
+    },
+    openSharingDialog() {
+      this.triggerDialog = !this.triggerDialog;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  padding-bottom: 6px;
+}
+.md-card-header {
+  padding: 0px 16px 0px 16px;
+  margin-bottom: 0px;
+}
+.md-card-content {
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.md-title {
+  margin-top: 6px;
+  font-size: 18px;
+  line-height: 120%;
+}
+.md-card-actions {
+  padding-top: 0;
+  padding-bottom: 0;
+}
+</style>
--- site/src/pages/Apodeltiosi.vue
+++ site/src/pages/Apodeltiosi.vue
@@ -1,8 +1,44 @@
 <template>
-  <Layout :title="'Αποδελτίωση τύπου'">
-      Προσεχώς
+  <Layout :title="title">   
+    <section>
+        <ApodeltiosiCard v-for="a in apodeltiosi" :key="a.href" :apodeltiosi="a"/>
+    </section>
   </Layout>
 </template>
 
 <script>
+import apodeltiosi from '~/data/apodeltiosi.json'
+import ApodeltiosiCard from '~/components/ApodeltiosiCard.vue'
+
+export default {
+  name: 'Apodeltiosi',
+  components: {
+    ApodeltiosiCard
+  },
+  data () {
+    return {
+      apodeltiosi,
+      title: 'Αποδελτίωση Τύπου'
+    }
+  },
+  metaInfo () {
+    return {
+      title: this.title,
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: this.title },
+        { property: 'og:image', content: '/diafaneia.jpg'},
+        { name: 'twitter:image', content: '/diafaneia.jpg'},
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: this.title },
+        { name: 'twitter:site', content: '@v_ntakouris' },
+        { name: 'twitter:creator', content: '@v_ntakouris' }
+      ]
+    }
+  }
+}
 </script>
+
+
+<style>
+</style>
