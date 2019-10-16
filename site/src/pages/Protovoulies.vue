25f75fdd2fec63225829ce3d4f36ed09961c4d16
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-10-16 22:32:44 +0300

    change title format

--- site/src/pages/Protovoulies.vue
+++ site/src/pages/Protovoulies.vue
@@ -5,4 +5,27 @@
 </template>
 
 <script>
+export default {
+  name: 'Protovoulies',
+  data () {
+    return {
+      title: 'Πρωτοβουλίες'
+    }
+  },
+  metaInfo () {
+    return {
+      title: this.title,
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: this.title },
+        { property: 'og:image', content: '/diafaneia.jpg'},
+        { name: 'twitter:image', content: 'https://basket12.gr/diafaneia.jpg'},
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: this.title },
+        { name: 'twitter:site', content: '@v_ntakouris' },
+        { name: 'twitter:creator', content: '@v_ntakouris' }
+      ]
+    }
+  }
+}
 </script>
