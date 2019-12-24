5864077ef8c13565277084e9a7cd07045a37a4c7
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-12-24 15:25:29 +0200

    strip images from head meta tags

--- site/src/pages/Index.vue
+++ site/src/pages/Index.vue
@@ -27,9 +27,7 @@ export default {
       meta: [
         { property: 'og:type', content: 'website' },
         { property: 'og:title', content: this.title },
-        { property: 'og:image', content: '/diafaneia.jpg'},
-        { name: 'twitter:image', content: 'https://basket12.gr/diafaneia.jpg'},
-        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:title', content: this.title },
         { name: 'twitter:site', content: '@v_ntakouris' },
         { name: 'twitter:creator', content: '@v_ntakouris' }
